be9c5915a3efacf5eed488eeabdb22687bf53b51
Niranjan <76111357+Niranjan-bit@users.noreply.github.com>
2025-03-13 16:34:00 +0530

    Update ChatbotApplicationTests.java

--- chatbot/src/test/java/niranjan_bit/ai/chatbot/ChatbotApplicationTests.java
+++ chatbot/src/test/java/niranjan_bit/ai/chatbot/ChatbotApplicationTests.java
@@ -1,13 +1,13 @@
+
 package niranjan_bit.ai.chatbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ChatbotApplication.class)
 class ChatbotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
