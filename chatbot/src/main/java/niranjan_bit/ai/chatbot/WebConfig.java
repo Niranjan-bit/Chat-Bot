290778a3e447812d1518435252be17ecdf877ea0
Niranjan <76111357+Niranjan-bit@users.noreply.github.com>
2025-03-13 18:19:57 +0530

    updated CORS

--- chatbot/src/main/java/niranjan_bit/ai/chatbot/WebConfig.java
+++ chatbot/src/main/java/niranjan_bit/ai/chatbot/WebConfig.java
@@ -2,16 +2,18 @@ package niranjan_bit.ai.chatbot;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://theaichatbot.netlify.app")  // Updated Netlify URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("https://theaichatbot.netlify.app") // Netlify frontend URL
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
